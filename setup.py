3a7f5993e8603b847755c1ee240f9a10c827b86b
Sergey <omelkovs@gmail.com>
2023-11-18 23:03:08 +0200

    changed setup.py and new requirements

--- setup.py
+++ setup.py
@@ -4,17 +4,21 @@ from setuptools import setup
 
 setup(
     name="pythermiagenesis",
-    version="0.1.7",
-    author="Johan Isaksson",
-    author_email="johan@generatorhallen.se",
+    version="0.1.8",
+    author="Johan Isaksson, extended by Sergey Omelkov",
+    author_email="johan@generatorhallen.se, omelkovs@gmail.com",
     description="Python wrapper for getting data from Thermia Mega and Inverter heat pumps \
         via Modbus TCP.",
     include_package_data=True,
-    url="https://github.com/cjne/pythermiagenesis",
+    url="https://github.com/aduc812/pythermiagenesis",
     license="MIT",
     packages=["pythermiagenesis"],
     python_requires=">=3.6",
-    install_requires=["pymodbustcp==0.1.10"],
+    # install_requires=["pymodbustcp==0.1.10"],
+    extras_require={
+        'TCP':[ "pymodbustcp==0.1.10" ],
+        'RTU':[ "pymodbus>=3.0.0" ]
+    },
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
