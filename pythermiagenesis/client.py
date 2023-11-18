704b5b25eaf8be57a6af6b63d33377b4ef79ec98
Sergey <omelkovs@gmail.com>
2023-11-18 14:23:35 +0200

    check if client already connected

--- pythermiagenesis/client.py
+++ pythermiagenesis/client.py
@@ -80,9 +80,10 @@ class ThermiaModbusRTUClient():
             )
 
     async def assure_connecion(self):
-        _LOGGER.info(f"Attempting to open a Modbus RTU serial port {self._port}")
-        if not await self._client.connect():
-            raise ThermiaConnectionError(f"Failed to open port {self._port}")
+        if not (client.connected) :
+            _LOGGER.info(f"Attempting to open a Modbus RTU serial port {self._port}")
+            if not await self._client.connect():
+                raise ThermiaConnectionError(f"Failed to open port {self._port}")
 
     async def write_single_coil(self, address, value):
         from pymodbus.exceptions import ModbusException
