d19f8f44eb26fb10bd5eb87c6793b838a033f775
Sergey <omelkovs@gmail.com>
2023-11-19 14:27:10 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # install_requires=["pymodbustcp==0.1.10"],
     extras_require={
         'TCP':[ "pymodbustcp==0.1.10" ],
-        'RTU':[ "pymodbus>=3.0.0" ]
+        'RTU':[ "pymodbus>=3.0.0","pyserial" ]
     },
     classifiers=[
         "License :: OSI Approved :: MIT License",
